92d0dad2d7e3502602709d614239988cbc96afa9
Matt Richardson <mrichardson23@gmail.com>
2013-09-17 12:56:10 -0400

    make script path absolute

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 320x180 -f 5" -o "/usr/lib/output_http.so -p 8090 -w /var/www" &
-python listener.py
+python /home/pi/udpListener/listener.py
